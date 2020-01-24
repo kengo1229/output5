bce40c34fdea49827d7703b31f327d0dcb12bfaa
kengo <tapioka.drink1221@gmail.com>
2020-01-24 14:23:52 +0900

    npm insyall 済み（なお、npmのバージョン上げたらエラーなく成功した） webpackにbrowsersync追加済み

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync({
+    proxy: {
+        target: "http://127.0.0.1:8000",
+    },
+    files: [
+        'resources/views/**/*.blade.php',
+    ],
+});
