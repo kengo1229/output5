1710f4ce4cf55af227bc372991975e14b918ae67
kengo <tapioka.drink1221@gmail.com>
2020-02-15 18:19:42 +0900

    no message

--- database/migrations/2020_01_18_131838_create_category_table.php
+++ database/migrations/2020_01_18_131838_create_category_table.php
@@ -17,8 +17,7 @@ class CreateCategoryTable extends Migration
         Schema::create('category', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('category_name');
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamps();
         });
     }
 
